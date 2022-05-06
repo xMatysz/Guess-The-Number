c44ba969fcd05c6b4664e15d0cd943a6ae3606e8
xMatysz <matizek56@gmail.com>
2022-05-06 20:43:21 +0200

    The entire program

--- Guess The Number/Program.cs
+++ Guess The Number/Program.cs
@@ -6,7 +6,50 @@ namespace GuessTheNumber
     {
         static void Main()
         {
+            bool correctAnswer = false;
+
+            Random random = new Random();
+            int randomNumber = random.Next(1,100);
+
+            ColorMessage("Witaj w grze \"Zgadnij liczbę!\"", ConsoleColor.Yellow);
+            ColorMessage("\nPodaj liczbę z przedziału 1..100", ConsoleColor.Red);
+
+            do
+            {
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out var number))
+                {
+                    if (number == randomNumber)
+                    {
+                        ColorMessage("Udało się zgadnąć liczbę!!", ConsoleColor.Green);
+                        correctAnswer = true;
+                    }
+                    else if (number < 1 || number > 100)
+                        ColorMessage("Wprowadzona liczba jest spoza przedziału", ConsoleColor.Red);
+                    else if (number > randomNumber)
+                        ColorMessage("Wylosowana liczba jest mniejsza", ConsoleColor.Red);
+                    else
+                        ColorMessage("Wylosowana liczba jest większa", ConsoleColor.Red);
+                }
+                else
+                    ColorMessage("Wprowadzona wartość nie jest liczbą całkowitą", ConsoleColor.Red);
+
+
+            } while (!correctAnswer);
+                
             
+        }
+        /// <summary>
+        /// Metoda wypisująca w konsoli podany tekst w podanym kolorze
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="color"></param>
+        static void ColorMessage(string input, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(input);
+            Console.ResetColor();
         }
     }
 }
